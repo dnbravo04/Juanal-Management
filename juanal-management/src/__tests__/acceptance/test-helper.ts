b203910578e2cbbde4e806bf135b345c286c0ac8
Diego Bravo A <digobravo@outlook.com>
2021-11-07 17:58:37 -0500

    First commit

---
+++ juanal-management/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {JuanalManagementApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new JuanalManagementApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: JuanalManagementApplication;
+  client: Client;
+}
